a469aaabeb03d3b4440f4db8ea052cfd59764fe6
Harsha <harshkn@gmail.com>
2023-01-26 21:19:01 +0530

    Page with Images

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,19 @@ use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
+    // let counter = use_state(|| 0);
+    // let onclick = {
+    //     let counter = counter.clone();
+    //     move |_| {
+    //         let value = *counter + 1;
+    //         counter.set(value);
+    //     }
+    // };
 
     html! {
         <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+            <img src="img/blocks.jpg" alt="blocks image"/> 
+            <img src="img/house.jpg" alt="house image"/>
         </div>
     }
 }
